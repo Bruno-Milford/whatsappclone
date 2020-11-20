b8b8652e1d8bfedbfeb1a7dd3f9be702cf1c1111
Trynda-LOL <40008415+Trynda-LOL@users.noreply.github.com>
2020-11-20 11:10:49 -0300

    Api
    Api feita com o db(firebase) e back(js)

---
+++ src/firebaseCongif.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDJeve3ANZ9RWfG5xhO-KXhXj_uOaJxaQ4",
+    authDomain: "whatsappclone-881f7.firebaseapp.com",
+    databaseURL: "https://whatsappclone-881f7.firebaseio.com",
+    projectId: "whatsappclone-881f7",
+    storageBucket: "whatsappclone-881f7.appspot.com",
+    messagingSenderId: "779572360723",
+    appId: "1:779572360723:web:cb77a75706ec91d35971e6"
+};
